364d8d8c4525f8724fcc1649cb8d7f9850cd696c
Shoxruz <forpk432@gmail.com>
2023-12-20 21:13:21 +0500

    style_contact

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -18,13 +18,13 @@ export const ContactUs = () => {
 
     return (
         <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-3'>
-            <label className='flex flex-col items-center'>Name
+            <label className='flex flex-col'>Name
                 <input className='min-w-full bg-transparent border rounded-md' type="text" name="user_name" />
             </label>
-            <label className='flex flex-col items-center'>Email
+            <label className='flex flex-col'>Email
                 <input className='min-w-full bg-transparent border rounded-md' type="email" name="user_email" />
             </label>
-            <label className='flex flex-col items-center'>Message
+            <label className='flex flex-col'>Message
                 <textarea className='border rounded-md bg-transparent min-w-full' name="message" />
             </label>
             <input type="submit" value="Send" className='border max-w-fit mx-auto py-2 px-5 rounded-xl mt-3'/>
